ea18b1b85646f7cb79a59b15c5807973e134653a
Muhammad Muzamil <muhammadmuzamilumrani@gmail.com>
2025-10-16 17:01:31 +0500

    Configure basePath and assetPrefix for production
    Add production configuration for basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: isProd ? '/your-repo-name' : '',
+  assetPrefix: isProd ? '/your-repo-name/' : '',
+};
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
